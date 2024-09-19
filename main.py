fc061965ec6d4caba83b79533e5bf8d5fb4ef9ca
omegadreamer <lolromik04@gmail.com>
2024-09-19 10:30:54 +0300

    Added encapsulation

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ class Employee:
         self.stage = stage  # месяц
         self.level = level  # уровень
         self.grade = grade  # должность
-        self.prem = False  # премия
+        self.__prize = False  # премия
 
     # получить
     def get_clear_salary(self):  # получить зарплату чистыми
         clear_salary = round(self.salary * 0.8)
-        if self.prem:
+        if self.__prize:
             clear_salary += 10000
         print(f"{clear_salary}$")
 
     def get_info(self):  # получить информацию
         print(f"Name - {self.fio}\n  Salary - {self.salary}$\n"
-              f"  Stage - {self.stage} Month\n  lvl - {self.level}\n"
-              f"  Grade - {self.grade}\n  Prem - {self.prem}")
+              f"  Stage - {self.stage} Month\n  Level - {self.level}\n"
+              f"  Grade - {self.grade}")
+        if self.__class__ != Hired:
+            print(f"  Prize - {self.__prize}\n")
 
     # изменить
     def set_grade(self):
@@ -32,29 +34,28 @@ class Employee:
                 print(f"{self.grade} grade of {self.fio} wasn't changed")
 
     # выдать
-    def give_prem(self):  # выдать премию
-        self.prem = True
+    def __give_prize(self):  # give prize | protected function
+        self.__prize = True
         print(f"prem for {self.fio} was gived")
 
 
 class Hired(Employee):
     def __init__(self, fio, salary, stage, level, grade, company, hired_time):
         Employee.__init__(self, fio, salary, stage, level, grade)
-        self.company = company
-        self.hired_time = hired_time
+        self.__company = company  # protected
+        self.__hired_time = hired_time  # protected
 
     def get_info(self):
-        print(f"Company - {self.company}\nHired for {self.hired_time} month")
+        print(f"Company - {self.__company}\nHired for {self.__hired_time} month")
         Employee.get_info(self)
+        print(f"\n")
 
 
 emp_1 = Employee("Omega Dreemurr", 2000, 64, "Senior", "Manager")
-emp_2 = Employee("Иванов Иван", 1350, 35, "Middle", "Team Leader")
-emp_3 = Employee("Дмитриев Игорь", 900, 13, "Junior", "C++ Devoleper")
-emp_4 = Hired("Корнев Роман", 900, 12, "Junior", "Py Devoleper", "Alpha", "24")
+emp_2 = Employee("Ivanov Ivan", 1350, 35, "Middle", "Team Leader")
+emp_3 = Employee("Simonov Dmitryi", 900, 13, "Junior", "C++ Devoleper")
+emp_4 = Hired("Kornev Roman", 900, 12, "Junior", "Py Devoleper", "Alpha", "24")
 emp_5 = Hired("Joseph Jostar", 900, 11, "Junior", "Js Devoleper", "Delta", "12")
 
-'''
 emp_1.get_info()
 emp_4.get_info()
-'''
